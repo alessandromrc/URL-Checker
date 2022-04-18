9da51dc52d7c6c8070c1a7b2e181b4c0056dc10f
Alessandro <66976091+alessandromrc@users.noreply.github.com>
2022-04-18 02:55:41 +0200

    removed csp

--- pages/_document.js
+++ pages/_document.js
@@ -1,25 +1,19 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
-import { CssBaseline } from "@nextui-org/react";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { CssBaseline } from '@nextui-org/react';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
     return {
       ...initialProps,
-      styles: <>{initialProps.styles}</>,
+      styles: <>{initialProps.styles}</>
     };
   }
 
   render() {
     return (
       <Html lang="en">
-        <Head>
-          {CssBaseline.flush()}
-          <meta
-            httpEquiv="Content-Security-Policy"
-            content="default-src 'self'; script-src 'self'; connect-src 'unsafe-eval'; style-src 'unsafe-inline'; font-src 'self'; img-src 'self'; script-src-elem 'self';"
-          />
-        </Head>
+        <Head>{CssBaseline.flush()}</Head>
         <body>
           <Main />
           <NextScript />
